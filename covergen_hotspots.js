86fdbd289091950c7a1f5a6ef540578534240361
root <root@prod-admin.(none)>
2011-11-02 15:01:02 +0000

    Added new files

---
+++ covergen_hotspots.js
@@ -0,0 +1,44 @@
+{[
+   "imprint" : "Maker Press"
+   "cover-template": "http://admin.makerpress.com/covergen/makerpress_template.png",
+   "width" : 400,
+   "height" : 600,
+   "hotspots": [
+      {
+         "element" : "callout-box",
+	     "text":  "Callout box"
+         "x" : 280,
+         "y" : 50,
+         "font-size": 20,
+         "font-color" : "#ffffff",
+         "font-name": "SansSerif-48"
+      },
+      {
+         "element" : "main-title",
+	     "text": "Title"
+         "x" : 50,
+         "y" : 100,
+         "font-size": 96,
+         "font-color" : "#ffffff",
+         "font-name": "SansSerif-48"
+      },
+      {
+         "element" : "sub-title",
+	     "text": "Sub Title",
+         "x" : 50,
+         "y" : 160,
+         "font-size": 72,
+         "font-color" : "#ffffff",
+         "font-name": "SansSerif-48"
+      },
+      {
+         "element" : "author-list",
+	     "text":  "Author list"
+         "x" : 50,
+         "y" : 220,
+         "font-size": 24,
+         "font-color" : "#ffffff",
+         "font-name": "SansSerif-48"
+      }
+   ]
+]}
